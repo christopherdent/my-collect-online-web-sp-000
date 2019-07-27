f757cb10e38f3bf6589d55cfc4b430ed6621ded7
Christopher Dent <christopherdent01@gmail.com>
2019-07-27 16:57:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
-
-
+def my_collect(array)
+    i = 0 
+    collection = []
+    while i < array.length 
+      yield(array[i])
+      collection.push(array[i])
+      i =+ 1
+  
+  end
+  p collection
+end 
+  
+  
+array = ["ruby", "javascript", "python", "objective-c"]
+my_collect(array) do |lang|
+  lang.upcase
+end 
+  
