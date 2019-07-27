8b769f2b40033ab8134902429d124c29e1a8024d
Christopher Dent <christopherdent01@gmail.com>
2019-07-27 17:04:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,8 @@ def my_collect(array)
     collection = []
     while i < array.length 
       yield(array[i])
-      i = i + 1
       collection << array[i]
-      
+      i = i + 1
     end
   p collection
 end 
